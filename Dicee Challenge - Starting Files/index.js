7f1018e6a83edaa7fdd508d14a1c475658c88ec5
Clancy Mendonca <72433654+smartbooty69@users.noreply.github.com>
2024-01-12 13:42:52 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,9 @@
+var randomN;
+randomN = Math.floor(Math.random() * 6) + 1;
+
+if (randomN=1){
+    document.querySelector('img')= 'images\dice1.png'
+}else if (randomN=2){
+    document.querySelector('img')= 'images/dice2.png'
+
+}
