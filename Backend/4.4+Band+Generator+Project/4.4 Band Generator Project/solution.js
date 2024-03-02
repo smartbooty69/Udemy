a508c4d0dd00744865af3115fd06e8e252a09ee5
Clancy Mendonca <clancymendonca@gmail.com>
2024-03-02 19:32:20 +0530

    4.4

--- Backend/4.4+Band+Generator+Project/4.4 Band Generator Project/solution.js
+++ Backend/4.4+Band+Generator+Project/4.4 Band Generator Project/solution.js
@@ -4,8 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("solution.ejs");
